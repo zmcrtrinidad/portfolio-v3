0eb93552c236aa2afb47a34c9212d840be0da5cb
Zinia Ma Consuelo Trinidad <66796707+zmcrtrinidad@users.noreply.github.com>
2023-02-16 15:46:47 +0800

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brown-20": "#9F765F",
+        "brown-50": "#83553B",
+        "brown-100": "#4B3122",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-graybrown": "linear-gradient(90deg, #6c5541 50%, #4b3122 50%)",
+      }),
+      fontFamily: {
+        kronaOne: ["Krona One", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        dots: "url('./assets/dots.png')",
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+    plugins: [],
+  }
+}
